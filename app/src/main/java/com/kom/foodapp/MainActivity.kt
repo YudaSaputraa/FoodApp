9bd1b7834761dcd804f397382cc5f80acb416c92
Komang Yuda Saputra <yudasaputra082@gmail.com>
2024-03-23 01:14:29 +0700

    Initial Project

---
+++ app/src/main/java/com/kom/foodapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.kom.foodapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kom.foodapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+}
