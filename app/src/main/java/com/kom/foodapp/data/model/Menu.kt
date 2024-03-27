f0609a6bedc77ef5082dd107559227d3c9bfa37d
Komang Yuda Saputra <yudasaputra082@gmail.com>
2024-03-28 01:24:21 +0700

    add database for carts

--- app/src/main/java/com/kom/foodapp/data/model/Menu.kt
+++ app/src/main/java/com/kom/foodapp/data/model/Menu.kt
@@ -1,14 +1,13 @@
 package com.example.foodapp.model
 
 import android.os.Parcelable
-import androidx.annotation.DrawableRes
 import kotlinx.parcelize.Parcelize
 import java.util.UUID
 
 @Parcelize
 data class Menu(
-    var id: String = UUID.randomUUID().toString(),
-    var image: String,
+    var id: String? = UUID.randomUUID().toString(),
+    var imageUrl: String,
     var name: String,
     var price: Double,
     val desc: String,
---
+++ app/src/main/java/com/kom/foodapp/data/source/local/database/dao/CartDao.kt
@@ -0,0 +1,30 @@
+package com.kom.foodapp.data.source.local.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.kom.foodapp.data.source.local.database.entity.CartEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CartDao {
+    @Query("SELECT * FROM carts")
+    fun getAllCarts(): Flow<List<CartEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCart(cart: CartEntity): Long
+
+    @Update
+    suspend fun updateCart(cart: CartEntity): Int
+
+    @Delete
+    suspend fun deleteCart(cart: CartEntity): Int
+
+    @Query("DELETE FROM carts")
+    suspend fun deleteAll()
+
+
+}
